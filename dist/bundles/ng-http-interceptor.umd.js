894134b5f2674ecbc18b06d821a33ce8b713ae1d
Andy Shin <andy.yoonyong@gmail.com>
2017-02-27 13:50:56 +0900

    [FIX]

--- dist/bundles/ng-http-interceptor.umd.js
+++ dist/bundles/ng-http-interceptor.umd.js
@@ -210,6 +210,11 @@ var InterceptableHttpProxyNoOverrideProviders = [
     }
 ];
 
+var __extends = (undefined && undefined.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var InterceptableHttp = (function (_super) {
     __extends(InterceptableHttp, _super);
     function InterceptableHttp(_backend, _defaultOptions) {
